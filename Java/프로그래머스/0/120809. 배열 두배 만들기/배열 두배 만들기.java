22ee1dfc275f7c352fae937c428ecc81df182514
jisooou <87888800+jisooou@users.noreply.github.com>
2024-03-18 14:05:15 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ Java/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] solution(int[] numbers) {
+        //ArrayOutOfBound가 나오지 않도록 
+        //배열을 처음~numbers의 길이만큼 초기화 시켜준다. 
         int[] answer = new int[numbers.length];
         
         for(int i = 0; i < numbers.length; i++){
