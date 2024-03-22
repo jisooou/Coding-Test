a42b535c604ca6fcd3f0afba4ecac3426633628c
jisooou <87888800+jisooou@users.noreply.github.com>
2024-03-22 10:41:37 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int money) {
+        //최대 아메리카노 잔 수 & 남는 돈
+        int[] answer = new int[2];
+        
+        answer[0] = money / 5500;
+            
+        answer[1] = money - (5500 * answer[0]);
+        
+        return answer;
+    }
+}
