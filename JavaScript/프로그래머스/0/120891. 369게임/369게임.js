b988adc8be17003a373a4db48b49a61c14672429
jisooou <87888800+jisooou@users.noreply.github.com>
2024-03-27 11:34:07 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120891. 369게임/369게임.js
+++ JavaScript/프로그래머스/0/120891. 369게임/369게임.js
@@ -1,8 +1,29 @@
+//1번째 풀이 
+
+function solution(order){
+    var answer = 0; 
+    
+    //문자열로 바꿔주기 
+    const numbers = order.toString();
+    
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] == 3 || numbers[i] == 6 || numbers[i] == 9){
+            answer = answer + 1;
+        }
+    }
+    
+    return answer;
+}
+
+//--------------------------------------------------------------//
+//2번째 풀이 
+
 //스프레드 연산자 
 //forEach() 함수
 
+/*
 function solution(order){
-    let answer = 0; 
+    var answer = 0; 
     
     [...String(order)].forEach(number => {
         if(number == 3 || number == 6 || number == 9) answer++; 
@@ -10,3 +31,4 @@ function solution(order){
     
     return answer;
 }
+*/
