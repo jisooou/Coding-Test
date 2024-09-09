a4722ae151ec149d9da1c8fda9c4d807c08d2114
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-09 22:33:42 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ Java/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int[] arr, int idx) {
         
+        //idx부터 for문을 돌면 된다
         for(int i = idx; i < arr.length; i++){
             
             if(arr[i] == 1){
