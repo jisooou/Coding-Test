806abb4176003f3cdbf411ff288c30cb0a07e303
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-09 15:29:25 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 19.21 ms, Memory: 78.2 MB -BaekjoonHub

--- Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -8,7 +8,9 @@ class Solution {
         for(int i = 0; i < seoul.length; i++){
             if(seoul[i].equals("Kim")){
                 answer = "김서방은 " + i + "에 있다";
+                break;
             }
+            
         }
         
         return answer;
