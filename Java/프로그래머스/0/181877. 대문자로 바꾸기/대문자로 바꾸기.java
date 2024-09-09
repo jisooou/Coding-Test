8d0c405317d7b0259183e6478f13cfc7dc7c6f9a
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-09 11:37:10 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,11 @@
+// 대문자로 바꾸기 : toUpperCase()
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = myString.toUpperCase();
+        
+        return answer;
+    }
+}
