06ebe524ebed34ac3e9b2a5403d6cb9312c31e26
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-10 20:57:36 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.57 ms, Memory: 76.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ Java/프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,14 +1,23 @@
-// 오름차순 정렬에 해당하는 import
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array) {
-       
-        Arrays.sort(array);
+        int answer = 0;
         
-        int mid = Math.round(array.length / 2);
-    
-        int answer = array[mid];
+        for(int i = 0; i < array.length; i++){
+            for(int j = i; j < array.length; j++){
+                
+                if(array[i] > array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp; 
+                    
+                }
+            }
+            System.out.println(array[i]);
+        }
+        
+        
+        answer = array[(array.length / 2)];
+        
         
         return answer;
     }
