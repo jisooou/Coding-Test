8db3123f42a6116ec659e1c280ea0aa601b31d4f
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-10 12:34:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.78 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        //제한 조건
+        if(phone_number.length() < 4 || phone_number.length() > 20){
+            System.out.println("phone_number 다시 입력");
+        }   
+        
+        for(int i = 0; i < phone_number.length()-4; i++){
+            answer += "*";
+        }
+        
+        answer += phone_number.substring(phone_number.length()-4);
+        
+        return answer;
+    }
+}
