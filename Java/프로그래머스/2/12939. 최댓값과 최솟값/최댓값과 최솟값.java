f1be19ce4ff6b0a105aad3d260ea5f76fad49653
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-10 13:28:21 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 14.15 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        //공백을 기준으로 split
+        String[] array = s.split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE; 
+        
+        for(int i = 0; i < array.length; i++){
+            //최소
+            if(Integer.parseInt(array[i]) < min){
+                min = Integer.parseInt(array[i]);
+            }
+            //최대
+            if(Integer.parseInt(array[i]) > max){
+                max = Integer.parseInt(array[i]);
+            }
+        }
+        
+        answer = Integer.toString(min) + " " + Integer.toString(max);
+        
+        return answer;
+    }
+}
