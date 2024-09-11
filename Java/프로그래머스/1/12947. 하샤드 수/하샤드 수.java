a952fa11cd2e470e1d7d1f278559a3cb1bd6345b
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-11 14:12:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.23 ms, Memory: 80.8 MB -BaekjoonHub

--- Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,39 +1,19 @@
-//x를 10,100,1000으로 나누면 몫+나머지 = x의각자릿수합 
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
         boolean error = false;
+        int sum = 0;
         
-        int a = x / 10; 
-        int b = x % 10;
-        int c = x / 100;
-        int d = x % 100; 
-        int e = x / 1000; 
-        int f = x % 1000; 
+        String[] num = Integer.toString(x).split("");
         
-        //1~99
-        if(x >= 1 && x <= 99){
-            if(x % (a+b) == 0){
-                return answer; 
-            }
+        for(int i = 0; i < num.length; i++){
+            sum = sum + Integer.parseInt(num[i]);
         }
-        //101~999
-        else if(x > 100 && x <= 999){
-            if(x % (c + (d/10) + (d%10)) == 0){
-                return answer; 
-            }
-        }
-        //1001~9999
-        else if(x > 1000 && x <= 9999){
-            if(x % (e + (f/100) + ((f%100)/10) + ((f %100)%10)) == 0 ){
-                return answer; 
-            }
-        }
-        else if(x == 100 || x == 1000 || x == 10000){
+        
+        if(x % sum == 0){
             return answer; 
         }
-        
-
         return error;
     }
+    
 }
