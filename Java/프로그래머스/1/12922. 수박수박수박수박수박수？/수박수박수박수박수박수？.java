46bf11e8f588ea64f8e7b897afc6eae88129c4c8
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-20 10:58:28 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 47.24 ms, Memory: 134 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int n) {
+        /*
+        1. 1만큼 돌을 때, "수" 리턴
+        2. n만큼 돌리면 n만큼 "수", "박"을 리턴
+        "박"을 넣는 방버버 : 인덱스가 홀수인 값에 "박"을 넣는다
+        
+        */
+        String answer = "";
+      
+        for(int i = 0; i < n; i+=2){
+            answer += "수";
+            for(int j = i+1; j < n; j+=2){
+                answer += "박";
+                break;
+            }
+        }
+     
+        return answer;
+    }
+}
