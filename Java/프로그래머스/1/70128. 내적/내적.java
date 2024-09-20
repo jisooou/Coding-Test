e84e0f6dbbc996cbedd32d370bc02c2a333de73b
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-20 10:19:15 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 76.3 MB -BaekjoonHub

--- Java/프로그래머스/1/70128. 내적/내적.java
+++ Java/프로그래머스/1/70128. 내적/내적.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(int[] a, int[] b) {
+        /*
+        배열 a와 배열 b의 길이가 같으므로 for문 1번만 돌려도O
+        */
         int sum = 0; 
         
         for(int i = 0; i < a.length; i++){
