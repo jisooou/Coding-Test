800e37174135566788cb604c7426bce094c1cc74
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-23 08:20:51 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 62.09 ms, Memory: 123 MB -BaekjoonHub

--- Java/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ Java/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,16 +1,17 @@
 class Solution {
     public String solution(int n) {
         /*
-        1. 1만큼 돌을 때, "수" 리턴
-        2. n만큼 돌리면 n만큼 "수", "박"을 리턴
+        1. 인덱스가 짝수 > "수" 리턴
+        2. 인덱스가 홀수 > "박" 리턴
         */
         String answer = "";
       
-        for(int i = 0; i < n; i+=2){
-            answer += "수";
-            for(int j = i+1; j < n; j+=2){
-                answer += "박";
-                break;
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                answer = answer + "수";
+            }
+            else{
+                answer = answer + "박";
             }
         }
      
