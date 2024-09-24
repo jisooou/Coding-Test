1845a11716d5edb14af3aeb6831fc433e6c4aea4
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-24 12:04:08 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.34 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String s) {
+        /*
+        단어 공백 기준으로 짝/홀수 인덱스
+        1.짝수번째 - 대문자
+        2.홀수번째 - 소문자
+        3.공백
+        */
+        String answer = "";
+        String[] array = s.split("");
+        int index = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            if(array[i].equals(" ")){
+                index = 0;
+            }
+            else if(index % 2 == 0){
+                array[i] = array[i].toUpperCase(); 
+                index++;
+            }
+            else if(index % 2 == 1){
+                array[i] = array[i].toLowerCase();
+                index++;
+            }
+                        
+            //index 값에 맞춰서 array에 정렬
+            answer += array[i];
+        }
+        
+        return answer;
+    }
+}
