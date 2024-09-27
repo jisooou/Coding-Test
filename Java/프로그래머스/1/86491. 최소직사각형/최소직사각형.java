bad28ea53cd645258e96abe11ac5d63c92675f3e
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-27 09:03:16 +0900

    [level 1] Title: 최소직사각형, Time: 1.43 ms, Memory: 85.6 MB -BaekjoonHub

--- Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,7 +3,7 @@ class Solution {
         /*
             (가로,세로) - 가로와 세로를 비교해서 더 작은 수를 가로에 다 모은 후, 
             가로에서 최댓값 * 세로에서 최댓값 = 답 
-        */
+        */ 
         int answer = 0; 
         
         for(int i = 0; i < sizes.length; i++){
@@ -17,27 +17,17 @@ class Solution {
         }
         
         //전체 배열에서 가로의 최대값 * 세로의 최대값
-        int max = 0; 
-        int min = 0;
+        int widthMax = 0; 
+        int heightMax = 0;
          for (int i = 0; i < sizes.length; i++) {
-            if (sizes[i][0] > max) {
-                max = sizes[i][0];
+            if (sizes[i][0] > widthMax) {
+                widthMax = sizes[i][0];
             }
-            if (sizes[i][1] > min) {
-                min = sizes[i][1];
+            if (sizes[i][1] > heightMax) {
+                heightMax = sizes[i][1];
             }
         }
-        // for(int x = 0; x < sizes.length; x++){
-        //     for(int y = 0; y < sizes[x].length; y++){
-        //         if(y[0] > max){
-        //             max = y[0];
-        //         }
-        //         if(y[1] < min){
-        //             min = y[1];
-        //         }
-        //     }
-        // }
-        answer = max * min; 
+        answer = widthMax * heightMax; 
         
         return answer;
     }
