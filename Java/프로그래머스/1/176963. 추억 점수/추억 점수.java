8af45bb30075aa81a1759da105dfafdc0a6af1ad
jisooou <87888800+jisooou@users.noreply.github.com>
2024-09-27 10:41:44 +0900

    [level 1] Title: 추억 점수, Time: 27.24 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        
+        /*
+        for문 3번 돌리면서 
+        photo 배열에 name 배열이 일치하는지 확인 
+        일치하는 횟수만큼 yearning에서 합 구하기 
+        */
+        int[] answer = new int[photo.length];
+        
+        for(int i = 0; i < photo.length; i++){
+            int sum = 0; 
+            
+            for(int j = 0; j < photo[i].length; j++){
+                String person = photo[i][j];
+                
+                for(int k = 0; k < name.length; k++){
+                    if(person.equals(name[k])){
+                        sum = sum + yearning[k];
+                    }
+                }
+            }
+            answer[i] = sum; 
+        }
+        return answer;
+    }
+}
