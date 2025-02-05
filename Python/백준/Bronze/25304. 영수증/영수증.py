b275871fa225bf677bc5911ad3b3a83b22fcea14
jisooou <87888800+jisooou@users.noreply.github.com>
2025-02-05 19:57:48 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,14 @@
+import sys
+
+sum = 0; 
+
+X = int(sys.stdin.readline().rstrip())
+N = int(sys.stdin.readline().rstrip())
+for _ in range(N):
+    a, b = map(int, sys.stdin.readline().split())
+    sum = sum + (a * b) 
+
+if(sum == X):
+	print("Yes")
+else:
+	print("No")
