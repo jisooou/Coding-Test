2dd3a8e69e8f2167966bd70ed4b152627a155d96
jisooou <87888800+jisooou@users.noreply.github.com>
2025-09-04 20:39:54 +0900

    [Silver IV] Title: 좋은 단어, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/3986. 좋은 단어/좋은 단어.py
+++ Python/백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -1,16 +1,16 @@
 import sys
 
-def is_good(word:str) -> bool:
-    #길이가 홀수 -> 절대 X
-    if len(word) % 2 == 1:
-        return False
+def is_good(word:str)->bool:
     stack = []
+    if len(word) % 2 == 1: 
+        return False 
     for ch in word: 
         if stack and stack[-1] == ch: 
             stack.pop()
         else:
             stack.append(ch)
-    return not stack # True 
+    return not stack 
+
 
 input = sys.stdin.readline
 num = int(input())
@@ -18,5 +18,5 @@ cnt = 0
 for _ in range(num):
     word = input().strip()
     if is_good(word):
-        cnt = cnt + 1 
+        cnt = cnt + 1
 print (cnt)
