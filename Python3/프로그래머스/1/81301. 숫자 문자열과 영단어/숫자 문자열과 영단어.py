27410f31b7ca7d05d7b33f16d665e8d0bf11c017
jisooou <87888800+jisooou@users.noreply.github.com>
2025-10-04 13:19:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.45 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,28 @@
+def solution(s):
+    answer = []
+    temp = ""
+#     딕셔너리 이용해서 매핑하기 
+    num_list = {
+        "zero" : "0",
+        "one" : "1",
+        "two" : "2",
+        "three" : "3",
+        "four" : "4",
+        "five" : "5", 
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8",
+        "nine" : "9"
+    }
+#     문자열을 순회 
+    for i in s: 
+        #i가 영어일 때 - 
+        if i.isalpha():
+            temp = temp + i 
+            if temp in num_list: 
+                answer.append(num_list[temp])
+                temp = ""
+        #i가 숫자일 때 
+        elif i.isdigit(): 
+            answer.append(i)
+    return int("".join(answer))
