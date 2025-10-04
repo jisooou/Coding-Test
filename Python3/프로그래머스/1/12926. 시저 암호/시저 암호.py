fc587394a4c4065527e133b64d37b49a51f2de07
jisooou <87888800+jisooou@users.noreply.github.com>
2025-10-04 12:42:16 +0900

    [level 1] Title: 시저 암호, Time: 2.31 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ Python3/프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,10 +1,17 @@
+#ord() 숫자로, chd() 알파벳으로
 def solution(s, n):
     result = []
+    #s를 어떻게 자를 것인가? 
     for i in s: 
-        if i.isupper(): 
-            result.append(chr((ord(i)-ord('A')+n)%26+ord('A')))
-        elif i.islower():
-            result.append(chr((ord(i)-ord('a')+n)%26+ord('a')))
+#         전부 대문자일 경우 
+        if i.isupper():
+            #A: 65, B: 66 / z -> a
+            result.append(chr(((ord(i)-ord('A')+n) % 26) + ord('A')))
+#             전부 소문자일 경우
+        elif i.islower(): 
+            result.append(chr(((ord(i)-ord('a')+n) % 26) + ord('a')))
+#             공백일 경우
         else: 
             result.append(i)
-    return "".join(result) #한줄에 출력할 수 있도록 한다. 
+    return("".join(result))
+            
