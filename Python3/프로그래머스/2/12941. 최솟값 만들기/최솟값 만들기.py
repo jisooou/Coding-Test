0984680529d3eed39ffcba28e3f5ccf348fd011a
jisooou <87888800+jisooou@users.noreply.github.com>
2025-10-11 22:01:02 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.39 ms, Memory: 9.26 MB -BaekjoonHub

--- Python3/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ Python3/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -2,10 +2,11 @@
 def solution(A,B):
     answer = 0
 
-    A_sort = sorted(A) #A[1,2,4]
-    B_sort = sorted(B, reverse=True) #B[5,4,4]
+    A_sort = sorted(A) #A[1,2,4] 오름차순
+    B_sort = sorted(B, reverse=True) #B[5,4,4] 내림차순
     
-    for a,b in zip(A_sort, B_sort):
+    #2개의 리스트를 같은 인덱스로 접근하기 위해=> zip()
+    for a,b in zip(A_sort, B_sort): 
         answer += a * b
     
     return answer
