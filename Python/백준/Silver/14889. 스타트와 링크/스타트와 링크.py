72c1709fb6bbfbb88458a897d53d200b1fb1c437
jisooou <87888800+jisooou@users.noreply.github.com>
2026-01-19 12:42:58 +0900

    [Silver I] Title: 스타트와 링크, Time: 5508 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ Python/백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input()) 
-S = [list(map(int, input().split())) for _ in range(N)]
+N = int(input())
+data = [list(map(int, input().split())) for _ in range(N)]
 
-visited = [False] * N 
+visited = [False] * N
 answer = float('inf')
 
 def dfs(idx, cnt):
     global answer
-    
-    if (cnt == N//2):
+
+    # 팀이 N/2명 찼으면 능력치 계산
+    if cnt == N // 2:
         start, link = 0, 0
         for i in range(N):
             for j in range(N):
                 if visited[i] and visited[j]:
-                    start += S[i][j]
+                    start += data[i][j]
                 elif not visited[i] and not visited[j]:
-                    link += S[i][j]
-        answer = min(answer, abs(start-link))
-        return 
-    
+                    link += data[i][j]
+        answer = min(answer, abs(start - link))
+        return
+
+    # 조합 생성
     for i in range(idx, N):
         if not visited[i]:
             visited[i] = True
-            dfs(i+1, cnt+1)
+            dfs(i + 1, cnt + 1)
             visited[i] = False
-    
-dfs(0,0)
+
+dfs(0, 0)
 print(answer)
-    
