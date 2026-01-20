ea8ed1dfd90a4c3e4151a7b0aa6b5fc0114e4443
jisooou <87888800+jisooou@users.noreply.github.com>
2026-01-20 17:28:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ Python/백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,27 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input()) # 3
-cnt = 0
+N = int(input())
+cnt = 0 
 
 for _ in range(N):
-    M = input().strip()
     stack = []
+    M = input().strip()
     is_group = True
-    
     for ch in M: 
         if not stack: 
             stack.append(ch)
         else: 
             if stack[-1] == ch: 
-                continue
-            elif ch in stack:
-                is_group = False
+                continue 
+            elif ch in stack: 
+                is_group = False 
                 break 
             else: 
                 stack.append(ch)
 
     if is_group: 
         cnt += 1 
-        
 print(cnt)
