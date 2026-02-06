e9d4af8aae7f5f72fe4afa53bbc47ed97b5567f6
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-06 11:46:31 +0900

    [Silver IV] Title: 암기왕, Time: 1428 ms, Memory: 229280 KB -BaekjoonHub

--- Python/백준/Silver/2776. 암기왕/암기왕.py
+++ Python/백준/Silver/2776. 암기왕/암기왕.py
@@ -1,14 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input()) 
+T = int(input())
 for _ in range(T):
-    N = int(input()) 
+    N = int(input())
     info = set(map(int, input().split()))
     M = int(input())
-    com_info = list(map(int, input().split()))
-    for i in com_info:
+    info_compare = list(map(int, input().split()))
+    
+    for i in info_compare:
         if i in info: 
             print(1)
-        else:
+        else: 
             print(0)
