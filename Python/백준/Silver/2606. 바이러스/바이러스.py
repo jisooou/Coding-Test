eb176cffef00a7eed352072773b073f3df0563d0
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-06 12:41:03 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 34944 KB -BaekjoonHub

--- Python/백준/Silver/2606. 바이러스/바이러스.py
+++ Python/백준/Silver/2606. 바이러스/바이러스.py
@@ -1,10 +1,8 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
-M = int(input()) #7
-N = int(input()) #6
-
+M = int(input())
+N = int(input())
 graph = [[] for _ in range(M+1)]
 
 for _ in range(N):
@@ -12,15 +10,16 @@ for _ in range(N):
     graph[a].append(b)
     graph[b].append(a)
     
-visited = [False] * (M+1)
-visited[1] = True
 queue = deque([1])
-cnt = 0 
-while queue: 
+visited = [False] * (M+1)
+visited[1] = True 
+cnt = 0
+
+while queue:
     curr_v = queue.popleft()
     for nxt_v in graph[curr_v]:
         if not visited[nxt_v]:
-            visited[nxt_v] = True
+            visited[nxt_v] = True 
             queue.append(nxt_v)
             cnt += 1
 print(cnt)
