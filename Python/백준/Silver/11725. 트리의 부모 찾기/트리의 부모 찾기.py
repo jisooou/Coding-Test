9770a4494a7d5e9fc4729156d0aafeabcce8ef29
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-09 10:53:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 256 ms, Memory: 67000 KB -BaekjoonHub

--- Python/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ Python/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -2,24 +2,24 @@ import sys
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-N = int(input()) #7
+N = int(input())
 graph = [[] for _ in range(N+1)]
 for _ in range(N-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-
+    
 visited = [False] * (N+1)
-visited[1] = True 
+visited[1] = True
 parent = [0] * (N+1)
 
 def dfs(curr_v):
     for nxt_v in graph[curr_v]:
         if not visited[nxt_v]:
-            visited[nxt_v] = True 
+            visited[nxt_v] = True
             parent[nxt_v] = curr_v
             dfs(nxt_v)
-            
+    
 dfs(1)
 for i in range(2, N+1):
     print(parent[i])
