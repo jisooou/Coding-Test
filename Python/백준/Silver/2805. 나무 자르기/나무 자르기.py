0d8c656cf0cf4e60fbc9855561533e6840704c17
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-10 15:37:56 +0900

    [Silver II] Title: 나무 자르기, Time: 2732 ms, Memory: 148224 KB -BaekjoonHub

--- Python/백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ Python/백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -7,17 +7,17 @@ trees = list(map(int, input().split()))
 left = 0
 right = max(trees)
 answer = 0
-while left <= right:
-    total = 0 
+while left <= right: 
+    total = 0
     mid = (left+right) // 2
     for i in trees:
-        if i > mid: 
+        if mid <= i: 
             total += (i-mid)
-            if total > M: # 시간 초과 방지
-                break 
-    if total >= M: # 나무 높이 기준선을 너무 낮게 잡은 것
+            if total > M: 
+                break
+    if total >= M: 
         answer = mid
         left = mid + 1
-    else: # 나무 높이 기준선을 너무 높게 잡은 것  
+    else:
         right = mid - 1
 print(answer)
