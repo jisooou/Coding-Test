5938fdef44622f81bc2364747216482950e9daff
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-11 18:39:10 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1860 ms, Memory: 101124 KB -BaekjoonHub

--- Python/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ Python/백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,35 +1,30 @@
 import sys
-import heapq
+from collections import deque
 input = sys.stdin.readline
-INF = int(1e9) # 무한대
 
 N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N+1)]
+
+#BFS : 큐를 사용 
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-dist = [INF] * (N+1)
+dist = [-1] * (N+1)
+queue = deque([X])
 dist[X] = 0
 
-heap = []
-heapq.heappush(heap, (0, X))
-
-while heap: 
-    curr_dist, curr_v = heapq.heappop(heap)
-    if curr_dist > dist[curr_v]:
-        continue
-        
-    for nxt_v in graph[curr_v]:
-        new_dist = curr_dist + 1 # 1이 추가
-        if new_dist < dist[nxt_v]:
-            dist[nxt_v] = new_dist
-            heapq.heappush(heap, (new_dist, nxt_v))
-            
+while queue: 
+    curr_n = queue.popleft()
+    for nxt_n in graph[curr_n]:
+        if (dist[nxt_n] == -1):
+            dist[nxt_n] = dist[curr_n] + 1
+            queue.append(nxt_n)
 found = False
 for i in range(1, N+1):
     if dist[i] == K: 
         print(i)
         found = True
-if not found:
+if not found: 
     print(-1)
+        
