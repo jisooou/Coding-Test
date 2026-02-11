711cea9015ee51e87d6eb10f402686c1d6a5e3ba
jisooou <87888800+jisooou@users.noreply.github.com>
2026-02-11 20:18:34 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 36264 KB -BaekjoonHub

--- Python/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ Python/백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,24 +1,22 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
+MAX = 100000
+visited = [False] * (MAX+1)
+visited[N] = True
+
 queue = deque()
 queue.append((N, 0))
 
-MAX = 100000
-visited = [False] * (MAX + 1)
-visited[N] = True 
-
 while queue: 
-    curr_N, cnt = queue.popleft()
-    if curr_N == K: 
+    curr_v, cnt = queue.popleft()
+    if curr_v == K: 
         print(cnt)
         break 
-        
-    for nxt_N in (curr_N-1, curr_N+1, curr_N*2):
-        if (0 <= nxt_N <= MAX) and not visited[nxt_N]:
-            visited[nxt_N] = True
-            queue.append((nxt_N, cnt+1))
+    for nxt_v in (curr_v-1, curr_v+1, curr_v*2):
+        if (0 <= nxt_v <= MAX) and not visited[nxt_v]:
+            visited[nxt_v] = True
+            queue.append((nxt_v, cnt+1))
