73417da3834ef60e95ce0b0bb3418bf3bbe2828f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-10 10:54:43 +0900

    [Silver II] Title: 유기농 배추, Time: 224 ms, Memory: 34968 KB -BaekjoonHub

--- Python/백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ Python/백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,31 @@
 from collections import deque
 T = int(input())
 for _ in range(T):
-    M, N, K = map(int, input().split())
-    graph = [[0] * M for _ in range(N)]
-    
-    for _ in range(K):
+    m, n, k = map(int, input().split())
+    dist = [[0]*m for _ in range(n)]
+    for _ in range(k):
         a, b = map(int, input().split())
-        graph[b][a] = 1
-        
+        dist[b][a] = 1
+    
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    
-    def bfs(cabb_x, cabb_y):
+        
+    def bfs(ax, ay):
         queue = deque()
-        queue.append((cabb_x, cabb_y))
-        graph[cabb_x][cabb_y] = 0
-    
-        while queue: 
+        queue.append((ax, ay))
+        dist[ax][ay] = 0 
+        while queue:
             x, y = queue.popleft()
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-            
-                if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
+                nx = dx[i] + x
+                ny = dy[i] + y
+                if 0 <= nx < n and 0 <= ny < m and dist[nx][ny] == 1: 
+                    dist[nx][ny] = 0 
                     queue.append((nx, ny))
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 1:
+    cnt = 0 
+    for i in range(n):
+        for j in range(m):
+            if dist[i][j] == 1:
                 bfs(i, j)
                 cnt += 1
-    print(cnt)
+    print(cnt)           
