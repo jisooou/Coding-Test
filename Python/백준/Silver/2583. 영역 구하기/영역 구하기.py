ee8a0e83f83bdc533f2ca32751533ffa63bd0bbc
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-11 20:49:27 +0900

    [Silver I] Title: 영역 구하기, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- Python/백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ Python/백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,36 +1,40 @@
 from collections import deque
-M, N, K = map(int, input().split())
-dist = [[0]*N for _ in range(M)]
+m, n, k = map(int, input().split())
+dist = [[0]*n for _ in range(m)]
 
-for _ in range(K):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for _ in range(k):
     x1, y1, x2, y2 = map(int, input().split())
     for y in range(y1, y2):
         for x in range(x1, x2):
             dist[y][x] = 1
 
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
-
-def bfs(r, c):
+def bfs(row, col):
     queue = deque()
-    queue.append((r, c))
-    dist[r][c] = 1
+    queue.append((row, col))
+    dist[row][col] = 1
     area = 1 
+    
     while queue: 
-        row, col = queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
-            nr = row + dx[i]
-            nc = col + dy[i]
-            if 0 <= nr < M and 0 <= nc < N and dist[nr][nc] == 0: 
-                dist[nr][nc] = 1
-                queue.append((nr, nc))
-                area += 1 
+            rx = dx[i] + x
+            cy = dy[i] + y
+        
+            if 0 <= rx < m and 0 <= cy < n and dist[rx][cy] == 0: 
+                dist[rx][cy] = 1
+                queue.append((rx, cy))
+                area += 1 #1로 채워진 area
     return area
+
 answer = []
-for i in range(M):
-    for j in range(N):
+for i in range(m):
+    for j in range(n):
         if dist[i][j] == 0: 
-            answer.append(bfs(i,j))
-answer.sort()  
+            answer.append(bfs(i, j))
+answer.sort()
+
 print(len(answer))
 print(*answer)
