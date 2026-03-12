b128dcb5340ef54224425230d360c9cc16122394
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-12 10:02:44 +0900

    [Silver I] Title: 쿼드트리, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ Python/백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,20 +1,18 @@
 N = int(input())
 dist = [list(map(int, input().strip())) for _ in range(N)]
-
 def q_tree(x, y, size):
     first = dist[x][y]
-    
     for i in range(x, x+size):
         for j in range(y, y+size):
-            if dist[i][j] != first: 
-                half_size = size // 2
-                return(
-                    "(" 
-                    + q_tree(x, y, half_size)
-                    + q_tree(x, y+half_size, half_size)
-                    + q_tree(x+half_size, y, half_size)
-                    + q_tree(x+half_size, y+half_size, half_size)
+            if dist[i][j] != first:
+                half = size // 2
+                return (
+                    "("
+                    +q_tree(x, y, half)
+                    +q_tree(x, y+half, half)
+                    +q_tree(x+half, y, half )
+                    +q_tree(x+half, y+half, half)
                     +")"
                 )
     return str(first)
-print(q_tree(0, 0, N))
+print(q_tree(0, 0, N))    
