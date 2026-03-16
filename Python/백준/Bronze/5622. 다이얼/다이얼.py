c0a80880a77c547b3ab5fcd18827d01a44e67906
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-16 21:34:38 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+s = input().strip()
+
+time = 0
+for ch in s:
+    if ch in "ABC":
+        time += 3
+    elif ch in "DEF":
+        time += 4
+    elif ch in "GHI":
+        time += 5
+    elif ch in "JKL":
+        time += 6
+    elif ch in "MNO":
+        time += 7
+    elif ch in "PQRS":
+        time += 8
+    elif ch in "TUV":
+        time += 9
+    elif ch in "WXYZ":
+        time += 10
+
+print(time)
