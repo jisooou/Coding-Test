2a868f6a6fda715cbd6a7f1a8f3d2129eb3f199c
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-17 14:11:17 +0900

    [Silver III] Title: 빈도 정렬, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ Python/백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -6,17 +6,16 @@ first = {}
 
 for i in range(N):
     num = nums[i]
-    
+
     if num not in freq: 
         freq[num] = 1
         first[num] = i
     else: 
         freq[num] += 1
-        
-l = []
-for num in freq:
-    l.append((-freq[num], first[num], num))
-l.sort()
-for freq, first, num in l: 
+answer = []
+for num in freq: 
+    answer.append((-freq[num], first[num], num))
+answer.sort()
+for freq, first, num in answer: 
     for _ in range(-freq):
         print(num, end = " ")
