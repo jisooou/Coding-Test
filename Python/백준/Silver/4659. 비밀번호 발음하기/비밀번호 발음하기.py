f055c6ae1ce2dcea5cf582cd8d1459c1c44c7d7c
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-18 22:05:24 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ Python/백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,33 +1,35 @@
 while(True):
-    s = input()
-    if s == "end":
-        break 
-        
     prev = ''
+    answer = True
     has_vowel = False
     vowel_cnt = 0
     cons_cnt = 0
-    ans_yes = True
-        
-    for ch in s: 
+
+    word = input()
+    if word == 'end':
+        break 
+    
+    for ch in word: 
         if ch == prev and ch not in 'eo':
-            ans_yes = False
+            answer = False    
+            
         if ch in 'aeiou':
             has_vowel = True
             vowel_cnt += 1
-            cons_cnt = 0 
+            cons_cnt = 0
         else: 
             cons_cnt += 1
-            vowel_cnt = 0 
-    
+            vowel_cnt = 0
+        
         if vowel_cnt == 3 or cons_cnt == 3: 
-            ans_yes = False 
-    
+            answer = False 
+            
         prev = ch 
-    if not has_vowel: 
-        ans_yes = False 
-
-    if ans_yes: 
-        print(f"<{s}> is acceptable.")
+        
+    if has_vowel == False: 
+        answer = False
+        
+    if answer: 
+        print(f"<{word}> is acceptable.")
     else: 
-        print(f"<{s}> is not acceptable.")
+        print(f"<{word}> is not acceptable.")
