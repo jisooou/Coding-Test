ee8e817940ba48237a0e52e1d90daa07d0b4042f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-24 23:35:01 +0900

    [Gold IV] Title: 연구소, Time: 3680 ms, Memory: 35132 KB -BaekjoonHub

--- Python/백준/Gold/14502. 연구소/연구소.py
+++ Python/백준/Gold/14502. 연구소/연구소.py
@@ -1,46 +1,44 @@
-from itertools import combinations
 from collections import deque
-import sys
+from itertools import combinations
 
-input = sys.stdin.readline
 N, M = map(int, input().split())
-wall = [list(map(int, input().split())) for _ in range(N)]
+data = [list(map(int, input().split())) for _ in range(N)]
 
-empty = []
+zero = []
 virus = []
 
 for i in range(N):
     for j in range(M):
-        if wall[i][j] == 0: 
-            empty.append((i, j))
-        elif wall[i][j] == 2: 
-            virus.append((i, j))
-            
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+        if data[i][j] == 0: 
+            zero.append((i,j))
+        elif data[i][j] == 2: 
+            virus.append((i,j))
 
 answer = 0
-
-for walls in combinations(empty, 3):
-    temp = [row[:] for row in wall]
+            
+for walls in combinations(zero, 3):
+    # 배열을 복사해 준다. 
+    temp = [row[:] for row in data]
     for x, y in walls: 
         temp[x][y] = 1
-        
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
     queue = deque(virus)
     while queue: 
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0 <= nx < N and 0 <= ny < M and temp[nx][ny] == 0:
+        
+            if 0 <= nx < N and 0 <= ny < M and temp[nx][ny] == 0: 
                 temp[nx][ny] = 2
                 queue.append((nx, ny))
-        
-    safe_cnt = 0 
+    safe_cnt = 0
     for i in range(N):
         for j in range(M):
-            if temp[i][j] == 0: 
+            if temp[i][j] == 0:
                 safe_cnt += 1
     answer = max(answer, safe_cnt)
-print(answer) 
+print(answer)
