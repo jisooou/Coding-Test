a7eff2f5d72287a8adf81c911012ce131ddb9c54
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-27 23:49:57 +0900

    [Gold V] Title: 트리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1068. 트리/트리.py
+++ Python/백준/Gold/1068. 트리/트리.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 parent = list(map(int, input().split()))
 erase_node = int(input())
@@ -7,25 +10,25 @@ root = 0
 
 for i in range(n):
     if parent[i] == -1: 
-        root = i
+        root = i 
     else: 
         tree[parent[i]].append(i)
-
+        
 def dfs(node):
     child_cnt = 0
     leaf_cnt = 0 
     
     for child in tree[node]:
-        if child == erase_node:
+        if child == erase_node: 
             continue
         child_cnt += 1
         leaf_cnt += dfs(child)
         
     if child_cnt == 0: 
-        return 1 
-    return leaf_cnt
+        return 1
+    return leaf_cnt 
 
-if erase_node == root: 
+if erase_node == root:
     print(0)
-else:
+else: 
     print(dfs(root))
