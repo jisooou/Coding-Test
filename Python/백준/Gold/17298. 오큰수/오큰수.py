a95b3912231e2fdff8488ab3032114519ad77bc4
jisooou <87888800+jisooou@users.noreply.github.com>
2026-03-31 17:01:59 +0900

    [Gold IV] Title: 오큰수, Time: 1112 ms, Memory: 140680 KB -BaekjoonHub

--- Python/백준/Gold/17298. 오큰수/오큰수.py
+++ Python/백준/Gold/17298. 오큰수/오큰수.py
@@ -1,15 +1,14 @@
 import sys
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
 n = int(input())
-num = list(map(int, input().split()))
-
 stack = []
 answer = [-1] * n
+num = list(map(int, input().split()))
 
-for i in range(n):
+for i in range(n): 
     while stack and num[stack[-1]] < num[i]:
-        idx = stack.pop()
-        answer[idx] = num[i]
+            idx = stack.pop()
+            answer[idx] = num[i]
     stack.append(i)
 print(*answer)
