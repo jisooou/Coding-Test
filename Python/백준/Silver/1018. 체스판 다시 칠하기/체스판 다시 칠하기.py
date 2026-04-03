cf43bced1c826b5d6906a6bd394b049b878a3290
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-03 09:15:19 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -3,26 +3,24 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 board = [input().strip() for _ in range(n)]
-
-answer = 64 # 최대 8*8
+answer = 64 # 8*8
 
 for x in range(n-7):
     for y in range(m-7):
-        white_start = 0
-        black_start = 0
+        white_b = 0
+        black_b = 0
         
         for i in range(x, x+8):
             for j in range(y, y+8):
-                if (i+j)%2 == 0:
+                if ((i+j) % 2 == 0): 
                     if board[i][j] != 'W':
-                        white_start += 1
+                        white_b += 1
                     if board[i][j] != 'B':
-                        black_start += 1
+                        black_b += 1
                 else: 
-                    if board[i][j] != 'B':
-                        white_start += 1
                     if board[i][j] != 'W':
-                        black_start += 1
-
-        answer = min(answer, white_start, black_start)
-print(answer)
+                        black_b += 1
+                    if board[i][j] != 'B':
+                        white_b += 1
+        answer = min(answer, black_b, white_b)
+print(answer)                    
