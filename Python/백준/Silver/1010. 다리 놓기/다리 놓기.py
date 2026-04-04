8c298f210756857ed79e8d409e10f980126b52d7
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-04 23:35:52 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ Python/백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,10 @@
+import sys
+import math
+input = sys.stdin.readline
+
+answer = 0
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    answer = math.comb(m, n)
+    print(answer)
