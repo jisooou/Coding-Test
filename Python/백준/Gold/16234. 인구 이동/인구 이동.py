8fe6a5c2a480280ae78615e85b858b2146ab1c4f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-06 21:50:34 +0900

    [Gold IV] Title: 인구 이동, Time: 5388 ms, Memory: 35016 KB -BaekjoonHub

---
+++ Python/백준/Gold/16234. 인구 이동/인구 이동.py
@@ -0,0 +1,52 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, L, R = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    
+    union = [(x, y)]
+    total = board[x][y]
+    
+    while queue: 
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                if L <= abs(board[x][y] - board[nx][ny]) <= R:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                    union.append((nx, ny))
+                    total += board[nx][ny]
+    return union, total    
+
+day = 0
+    
+while True: 
+    visited = [[False] * N for _ in range(N)]
+    move = False 
+   
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                union, total = bfs(i, j)
+                
+                if len(union) > 1:
+                    move = True
+                    new_move = total // len(union)
+                    for x, y in union: 
+                        board[x][y] = new_move
+    if not move: 
+        break 
+        
+    day += 1
+print(day)
