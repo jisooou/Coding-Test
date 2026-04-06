96c326ea3cebf70018f381f0f68535cf074bd9cc
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-06 12:56:59 +0900

    [Silver III] Title: 숫자 정사각형, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ Python/백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -1,14 +1,14 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline 
 
 n, m = map(int, input().split())
 board = [input().strip() for _ in range(n)]
 
-answer = 1
+answer = 1 #최소 1*1
 
 for i in range(n):
     for j in range(m):
         for k in range(min(n-i, m-j)):
-            if board[i][j] == board[i][j+k] == board[i+k][j] == board[i+k][j+k]:
+            if board[i][j] == board[i+k][j] == board[i][j+k] == board[i+k][j+k]:
                 answer = max(answer, (k+1)**2)
 print(answer)
