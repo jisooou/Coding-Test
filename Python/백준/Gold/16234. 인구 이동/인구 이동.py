74f536378ae74ff5fff1f0c12d105c5ace0d4394
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-08 10:08:58 +0900

    [Gold IV] Title: 인구 이동, Time: 5088 ms, Memory: 35148 KB -BaekjoonHub

--- Python/백준/Gold/16234. 인구 이동/인구 이동.py
+++ Python/백준/Gold/16234. 인구 이동/인구 이동.py
@@ -13,40 +13,40 @@ def bfs(x, y):
     queue.append((x, y))
     visited[x][y] = True
     
-    scope = [(x, y)]
+    union = [(x, y)]
     total = board[x][y]
     
     while queue: 
         x, y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
+            nx= x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 if L <= abs(board[x][y] - board[nx][ny]) <= R:
-                    visited[nx][ny] = True
+                    visited[nx][ny] = True 
                     queue.append((nx, ny))
-                    scope.append((nx, ny))
+                    union.append((nx, ny))
                     total += board[nx][ny]
-    return scope, total    
+    return union, total 
 
-day = 0
-    
+day = 0 
+
 while True: 
     visited = [[False] * N for _ in range(N)]
     move = False 
-   
+    
     for i in range(N):
         for j in range(N):
             if not visited[i][j]:
-                scope, total = bfs(i, j)
+                union, total = bfs(i, j)
                 
-                if len(scope) > 1:
+                if len(union) > 1:
                     move = True
-                    new_move = total // len(scope)
-                    for x, y in scope: 
+                    new_move = total // len(union)
+                    for x, y in union: 
                         board[x][y] = new_move
     if not move: 
         break 
         
     day += 1
-print(day)
+print(day)        
