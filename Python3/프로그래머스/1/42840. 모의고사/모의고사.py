74366ff00ffe010263c3959406a2313333d505eb
jisooou <87888800+jisooou@users.noreply.github.com>
2026-04-30 16:17:11 +0900

    [level 1] Title: 모의고사, Time: 3.98 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/1/42840. 모의고사/모의고사.py
+++ Python3/프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,25 +1,20 @@
 def solution(answers):
-    answer = []
-    
     first = [1, 2, 3, 4, 5]
     second = [2, 1, 2, 3, 2, 4, 2, 5]
     third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+    result = []
     
-    first_cnt = 0
-    second_cnt = 0
-    third_cnt = 0
-    
-    for i in range(len(answers)):
-        if answers[i] == first[i%len(first)]:
-            first_cnt += 1
-        if answers[i] == second[i%len(second)]:
-            second_cnt += 1
-        if answers[i] == third[i%len(third)]:
-            third_cnt += 1
-            
-    score = [first_cnt, second_cnt, third_cnt]
-    
-    for s in range(len(score)):
-        if score[s] == max(score):
-            answer.append(s+1)
-    return answer
+    for idx, answer in enumerate(answers): 
+        if answer == first[idx%len(first)]:
+            score[0] += 1
+        if answer == second[idx%len(second)]:
+            score[1] += 1
+        if answer == third[idx%len(third)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score): 
+        if s == max(score):
+            result.append(idx+1)
+        
+    return result
