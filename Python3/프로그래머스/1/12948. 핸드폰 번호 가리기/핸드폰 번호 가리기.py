00032309ea02b16c2497052adc4052d700802221
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-06 12:55:43 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ Python3/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,4 +1,4 @@
 def solution(phone_number):
-    phone_number_star = "*" * len(phone_number[:-4]) #0103333 -> *******
-    answer = phone_number_star + phone_number[-4:]
-    return answer
+    star = "*"*(len(phone_number)-4)
+    number = phone_number[-4:]
+    return star + number
