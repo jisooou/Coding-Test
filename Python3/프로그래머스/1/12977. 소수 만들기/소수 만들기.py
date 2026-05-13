de4666a7617c72b74a45456167108db37ff7c92c
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-13 16:41:17 +0900

    [level 1] Title: 소수 만들기, Time: 26.21 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ Python3/프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,14 +1,14 @@
 from itertools import combinations
 import math
 def solution(nums):
-    cnt = 0
-    for comb in combinations(nums, 3):
-        total = sum(comb)
+    answer = 0
+    for num in combinations(nums, 3):
+        total = sum(num)
         is_prime = True
         for i in range(2, int(math.sqrt(total))+1):
-            if total%i==0:
+            if total%2==0 or total%i==0:
                 is_prime = False
-                break
         if is_prime:
-            cnt += 1
-    return cnt
+            answer += 1
+
+    return answer
