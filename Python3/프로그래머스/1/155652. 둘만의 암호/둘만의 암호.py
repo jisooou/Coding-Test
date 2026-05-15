0a0ad5591858985b65e0376647bcdd709df5f767
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-15 15:58:26 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ Python3/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,9 +1,9 @@
 def solution(s, skip, index):
     answer = ''
     alpha = 'abcdefghijklmnopqrstuvwxyz'
-    for i in skip:
-        alpha = alpha.replace(i, '')
-    for word in s:
-        idx = (alpha.index(word)+index)%len(alpha)
+    for sk in skip:
+        alpha = alpha.replace(sk, '')
+    for i in s: 
+        idx = (alpha.index(i)+index)%len(alpha)
         answer += alpha[idx]
     return answer
