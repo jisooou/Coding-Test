a4c4aad5494a906b7baa6f5d30a92060c3e0828e
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-18 16:09:55 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 9 MB -BaekjoonHub

--- Python3/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ Python3/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,10 +1,9 @@
-#bin()
 def solution(n):
-    cnt = bin(n).count('1')
-    num = n + 1
+    cnt_bin = bin(n).count('1')
+    nxt = n + 1
     while True:
-        num_cnt = bin(num).count('1')
+        nxt_cnt = bin(nxt).count('1')
+        if cnt_bin == nxt_cnt:
+            return nxt
+        nxt += 1
         
-        if cnt == num_cnt:
-            return num 
-        num += 1
