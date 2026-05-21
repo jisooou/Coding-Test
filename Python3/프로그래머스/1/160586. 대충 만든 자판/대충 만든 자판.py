61890e4a37fd50e2ccba0958cfbf748bca0acdba
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-21 12:16:09 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.08 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,21 +1,20 @@
 def solution(keymap, targets):
     answer = []
-#     각 알파벳이 최소 몇번 눌러야하는지 
-    press_alpha = {}
+    alpha = {}
     for k in keymap:
-        for idx, ch in enumerate(k):
+        for idx, word in enumerate(k):
             press = idx + 1
-            if ch not in press_alpha:
-                press_alpha[ch] = press
+            if word in alpha:
+                alpha[word] = min(alpha[word], press)
             else:
-                press_alpha[ch] = min(press_alpha[ch], press)
-                
-    for t in targets: 
+                alpha[word] = press
+    
+    for t in targets:
         total = 0
-        for ch in t: 
-            if ch not in press_alpha:
+        for target_word in t:
+            if target_word not in alpha:
                 total = -1
                 break
-            total += press_alpha[ch]
+            total += alpha[target_word]
         answer.append(total)
-    return answer
+    return answer    
