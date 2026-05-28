2d82d596c97a50b2726354324e3ab85e85b45d68
jisooou <87888800+jisooou@users.noreply.github.com>
2026-05-28 22:24:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 594.23 ms, Memory: 12.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,30 +1,20 @@
-# 수행시간- O(n^2): 초과 -> O(n√n): 개선
-
 import math
 def solution(number, limit, power):
-    # 1. 1부터 number까지 수를 돈다. 
-    # 2. 하나씩 돌면서 약수의 개수를 구한다. 
-    #     1. 약수의 개수를 어떻게 구하는가? 
-    #     예) 6이라면, 1부터 6까지 돌면서 6과 나눠떨어지는지 확인한다. 
-    #         1. 나눠떨어지면, 나눠떨어지는 수를 구한다. 
-    # 3. 만약 이때 나눠떨어지는 수들이 power보다 크면 power을 사용한다. 
-    # 4. 나눠떨어지는 수와 power 대체된 수가 있으면 모두 더해서 result를 구한다.
-    answer = 0
-    for i in range(number+1): #print(i)->1,2,3,4,5
-#         예: i = 4 
+    num_cnt = []
+    for num in range(1, number+1): 
         cnt = 0
-#      for문을 다 돌릴 경우: 시간 초과 
-#      sqrt()한 수가까지만 돌린다. 예) i가 4일때, k는 1~3까지. 즉 1,2만 돈다. 
-#      i가 k로 나눠떨어지면 약수인데, k가 1일 때 2 증가(1,4 쌍으로 나옴) 
-#      k가 2일 때 1 증가: 즉 제곱수라는 말이다. 2 = 4 // 2 -> 제곱수
-        for k in range(1, int(math.sqrt(i)) + 1): #1,2,3,4
-            if(i % k == 0): #나눠떨어지면 약수 
-                if(k == i // k): #제곱수일 때 
+        for i in range(1, int(math.sqrt(num))+1): 
+            if num % i == 0:
+                if i * i == num:
                     cnt += 1
                 else:
                     cnt += 2
-        if cnt > limit: 
-            cnt = power
-        answer += cnt
+        num_cnt.append(cnt)
     
-    return answer
+    total = 0
+    for n in num_cnt:
+        if n > limit:
+            total += power
+        else:
+            total += n  
+    return total
