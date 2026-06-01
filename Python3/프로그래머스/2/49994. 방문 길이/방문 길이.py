559431a60c8e95a402d15223fb954f7f16ba20cf
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-01 15:22:40 +0900

    [level 2] Title: 방문 길이, Time: 0.11 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ Python3/프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,22 +1,19 @@
 def solution(dirs):
-    x, y = 0, 0
-    visited = set()
     move = {
         'U':(0,1),
         'D':(0,-1),
         'R':(1,0),
         'L':(-1,0)
     }
-    
-    for d in dirs:
-        dx, dy = move[d]
-        nx = x + dx
-        ny = y + dy
+    visit = set()
+    x, y = 0, 0
+    for d in dirs: 
+        dx, dy = move[d] #0, 1
+        nx = dx + x
+        ny = dy + y
         if nx<-5 or nx>5 or ny<-5 or ny>5:
             continue
-        
-        visited.add((x, y, nx, ny))
-        visited.add((nx, ny, x, y))
+        visit.add((x, y, nx, ny))
+        visit.add((nx, ny, x, y))
         x, y = nx, ny
-        
-    return len(visited)//2
+    return len(visit)//2
