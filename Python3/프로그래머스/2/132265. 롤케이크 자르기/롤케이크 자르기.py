9c2ba5d7a0cc643d0102873f22c5142ec51c5846
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-03 10:04:58 +0900

    [level 2] Title: 롤케이크 자르기, Time: 228.84 ms, Memory: 51.7 MB -BaekjoonHub

--- Python3/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ Python3/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -5,16 +5,15 @@ def solution(topping):
             right[t] = 1
         else:
             right[t] += 1
-    
-    cnt = 0
+            
     left = set()
+    cnt = 0
     for t in topping:
         left.add(t)
         right[t] -= 1
-        
         if right[t] == 0:
             del right[t]
-            
+        
         if len(left) == len(right):
             cnt += 1
     return cnt 
