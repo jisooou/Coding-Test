66850d899413b24dafe70277d9b8b5ba3b5db87b
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-05 15:42:59 +0900

    [level 2] Title: 더 맵게, Time: 1654.40 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while len(scoville)>=2 and scoville[0]<K:
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        new_score = a + (b*2)
+        heapq.heappush(scoville, new_score)
+        answer += 1
+    if scoville[0] < K:
+        return -1
+    else:
+        return answer
