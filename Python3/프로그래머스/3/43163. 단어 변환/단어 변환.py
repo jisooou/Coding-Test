849d8654d21e0ecce2f55c658644597032306eec
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-12 16:55:18 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ Python3/프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,24 +1,24 @@
 from collections import deque
 def solution(begin, target, words):
+    visited = [False]*len(words)
+    queue = deque()
+    queue.append((begin, 0))
+    
     if target not in words:
         return 0
     
-    visited = [False] * len(words)
-    
-    queue = deque()
-    queue.append((begin, 0))
     while queue:
-        word, cnt = queue.popleft()
-        if word == target:
+        current_word, cnt = queue.popleft()
+        if current_word == target:
             return cnt
         
         for i in range(len(words)):
-            nxt_word = words[i]
+            new_word = words[i]
             diff = 0
-            for j in range(len(word)):
-                if word[j] != nxt_word[j]:
+            for j in range(len(begin)):
+                if new_word[j] != current_word[j]: 
                     diff += 1
-            if not visited[i] and diff == 1:
+            if not visited[i] and diff==1:
                 visited[i] = True
-                queue.append((nxt_word, cnt+1))
+                queue.append((new_word, cnt+1))
     return 0
