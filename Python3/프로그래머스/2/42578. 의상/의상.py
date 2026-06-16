fe71289c9c2879184160f6ad9c65d6a314fa5497
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-17 01:13:01 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/42578. 의상/의상.py
+++ Python3/프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,11 @@
 def solution(clothes):
-    collect = {}
-    for i in clothes:
-        if i[1] not in collect:
-            collect[i[1]] = 1
+    clothe = {}
+    for wear, type in clothes:
+        if type not in clothe:
+            clothe[type] = 1
         else:
-            collect[i[1]] += 1
-    
+            clothe[type] += 1
     answer = 1
-    for i in collect.values():
+    for i in clothe.values():
         answer *= (i+1)
     return answer-1
