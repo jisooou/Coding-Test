fdad1f191df3d886d9baac4be5f6286affe32c58
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-17 11:02:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.10 ms, Memory: 34.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,5 @@
 from collections import Counter
 def solution(participant, completion):
     remain = Counter(participant) - Counter(completion)
-    for answer in remain:
-        return answer
+    for i in remain.keys():
+        return i
