8d05f4a8be9ec7173bf8b4202358b173fb9695c8
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-18 17:17:16 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,11 @@
-#완전탐색 문제
-#조건1.brown = 세로는 무조건 3이상이어야 한다. 
-#조건2.return 값에 [4,3]-[x,y] : (x-2)*(y-2) = yellow
-
-import math 
+import math
 def solution(brown, yellow):
-    total = brown + yellow #12
-    #세로를 기준으로 한다. 세로 무조건 3이상. 
-    #약수 쌍 구할 때: 제곱수까지만 순회
-    #조건1
-    for height in range(3, int(math.sqrt(total)+1)):
-        if total % height == 0: 
-            width = total // height
-            #조건2
-            if (width-2)*(height-2) == yellow: 
-                return [width, height]
+    collect = []
+    box = brown + yellow
+    for i in range(1, int(math.sqrt(box))+1):
+        if box % i == 0: 
+            collect.append(((box//i), i))
+    
+    for b, y in collect:
+        if (b-2)*(y-2) == yellow:
+            return [b, y]
