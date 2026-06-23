5881e385cfbe239f49b2d2f6064ab058a7d14563
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-23 15:37:26 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,4 +1,3 @@
-# 최단거리를 구하는 문제가 아니다
 def solution(n, computers):
     visited = [False] * n 
     def dfs(computer):
@@ -6,9 +5,10 @@ def solution(n, computers):
         for node in range(n):
             if computers[computer][node] == 1 and not visited[node]:
                 dfs(node)
+                
     answer = 0
-    for i in range(n):
-        if not visited[i]:
-            dfs(i)
+    for computer in range(n):
+        if not visited[computer]:
+            dfs(computer)
             answer += 1
-    return answer 
+    return answer
