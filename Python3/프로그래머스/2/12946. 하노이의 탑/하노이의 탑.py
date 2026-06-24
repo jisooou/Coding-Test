aaa9b2a52af9aa2afb4606b859a002691ba7e03f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-25 00:11:37 +0900

    [level 2] Title: 하노이의 탑, Time: 5.08 ms, Memory: 17.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = []
+    def hanoi(num, start, end, mid):
+        if num == 1:
+            answer.append([start, end])
+            return 
+        
+        hanoi(num-1, start, mid, end)
+        answer.append([start, end])
+        hanoi(num-1, mid, end, start)
+    hanoi(n, 1, 3, 2)
+    return answer
