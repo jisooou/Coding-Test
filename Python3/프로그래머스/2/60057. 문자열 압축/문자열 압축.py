d0c02440df6adea8821da70e3d8060fd3f84780a
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-27 15:38:55 +0900

    [level 2] Title: 문자열 압축, Time: 1.13 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,25 @@
+def solution(s):
+    answer = len(s)
+    for unit in range(1, len(s)//2+1):
+        prev = s[0:unit]
+        cnt = 1
+        compress = ''
+        for i in range(unit, len(s), unit):
+            current = s[i:i+unit]
+            if prev == current:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    compress += str(cnt) + prev
+                else:
+                    compress += prev
+                cnt = 1
+                prev = current
+        if cnt > 1:
+            compress += str(cnt) + prev
+        else:
+            compress += prev
+        answer = min(answer, len(compress))
+    return answer
+                
+            
