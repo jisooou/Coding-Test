297e000f4621fda600476267fd62bf8d2472c4bb
jisooou <87888800+jisooou@users.noreply.github.com>
2026-06-30 15:47:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.10 ms, Memory: 11.7 MB -BaekjoonHub

--- Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ Python3/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,22 +1,33 @@
 def solution(new_id):
+    #1
     s = new_id.lower()
     
-    allow = "abcdefghijklmnopqrstuvwxyz0123456789-_."
+    #2
+    allow = 'abcdefghijklmnopqrstuvwxyz0123456789-_.'
     temp = ''
     for i in s:
         if i in allow:
-            temp += i 
+            temp += i
     s = temp 
     
+    #3
     while '..' in s:
         s = s.replace('..', '.')
-    s = s. strip('.') 
+    
+    #4
+    s = s.strip('.')
+    
+    #5
     if s == '':
         s = 'a'
+        
+    #6
     if len(s) >= 16:
         s = s[:15]
         s = s.rstrip('.')
+    
+    #7
     while len(s)<3:
         s += s[-1]
-    
+        
     return s
