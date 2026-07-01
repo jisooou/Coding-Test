5c144cf111a8648711b295dd3d500083ce213756
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-01 15:16:02 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,13 @@
 import math
 def solution(brown, yellow):
-    collect = []
-    box = brown + yellow
-    for i in range(1, int(math.sqrt(box))+1):
-        if box % i == 0: 
-            collect.append(((box//i), i))
-    
-    for b, y in collect:
-        if (b-2)*(y-2) == yellow:
-            return [b, y]
+    #규칙을 찾는다 
+    answer = []
+    carpet = brown + yellow
+    for h in range(1, int(math.sqrt(carpet))+1):
+        if carpet % h == 0:
+            w = carpet // h
+        if (w-2)*(h-2) == yellow:
+            if w >= h:
+                answer.append(w)
+                answer.append(h)
+    return answer     
