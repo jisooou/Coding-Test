2d3ad19fcf102cf33791b9b43fe93e44c26bcd68
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-05 19:50:43 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ Python3/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,15 +1,14 @@
 def solution(n):
     answer = ''
-    while n > 0 :
-        remain = n % 3
-        if remain == 1: 
+    while n > 0:
+        remain = str(n % 3)
+        if remain == '1':
             answer = '1' + answer
             n //= 3
-        elif remain % 3 == 2:
+        elif remain == '2':
             answer = '2' + answer
             n //= 3
-        elif remain % 3 == 0:
+        elif remain == '0':
             answer = '4' + answer
             n = n // 3 - 1
-    
     return answer
