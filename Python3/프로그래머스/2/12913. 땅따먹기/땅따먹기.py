592d575a86bd0a4959e8e0889168018b23100b41
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-08 17:37:41 +0900

    [level 2] Title: 땅따먹기, Time: 102.95 ms, Memory: 43.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ Python3/프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,7 +1,9 @@
 def solution(land):
+    dp = [[0]*4 for _ in range(len(land))]
+    dp[0] = land[0]
     for i in range(1, len(land)):
-        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
-        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
-        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
-        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
-    return max(land[-1])
+        dp[i][0] = land[i][0] + max(dp[i-1][1], dp[i-1][2], dp[i-1][3])
+        dp[i][1] = land[i][1] + max(dp[i-1][0], dp[i-1][2], dp[i-1][3])
+        dp[i][2] = land[i][2] + max(dp[i-1][0], dp[i-1][1], dp[i-1][3])
+        dp[i][3] = land[i][3] + max(dp[i-1][0], dp[i-1][1], dp[i-1][2])
+    return max(dp[-1])
