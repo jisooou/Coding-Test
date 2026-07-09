9ad1384c26889e3d6994e29ac094cb3db1c5781b
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-09 16:08:07 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.22 ms, Memory: 11.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -3,25 +3,26 @@ def solution(n, wires):
     answer = n
     for cut_x, cut_y in wires:
         graph = [[] for _ in range(n+1)]
-        for a, b in wires:
+        for a,b in wires:
             if cut_x == a and cut_y == b:
                 continue
             graph[a].append(b)
             graph[b].append(a)
-        
+            
         queue = deque()
-        queue.append(1)
         visited = [False] * (n+1)
+        queue.append(1)
         visited[1] = True
         cnt = 1
-        
+
         while queue:
-            curr_node = queue.popleft()
-            for nxt_node in graph[curr_node]:
-                if not visited[nxt_node]:
-                    visited[nxt_node] = True
-                    queue.append(nxt_node)
+            current = queue.popleft() 
+            for nxt in graph[current]:
+                if not visited[nxt]:
+                    visited[nxt] = True
+                    queue.append(nxt)
                     cnt += 1
-        remain = n - cnt
+
+        remain = n-cnt
         answer = min(answer, abs(cnt-remain))
     return answer
