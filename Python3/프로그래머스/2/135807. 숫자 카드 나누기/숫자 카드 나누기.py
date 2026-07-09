87cc9a9ac818b2dfb19acc4aee4919573c23fc78
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-09 15:02:29 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 59.45 ms, Memory: 54.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ Python3/프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,25 +1,26 @@
 import math
 def solution(arrayA, arrayB):
-    def get_gcd(arr):
-        g = arr[0]
-        for num in arr[1:]:
-            g = math.gcd(g, num)
+    #A의 최대공약수 -> B로 안 나눠떨어지는지
+    #B의 최대공약수 -> A로 안 나눠떨어지는지
+    def get_gcd(array):
+        g = array[0]
+        for i in array[1:]:
+            g = math.gcd(g, i)
         return g
     
-    def is_valid(g, arr):
-        for i in arr:
+    def is_valid(g, array):
+        for i in array:
             if i % g == 0:
                 return False
         return True
-        
+    
     a = get_gcd(arrayA)
     b = get_gcd(arrayB)
     
     answer = 0
-    
     if is_valid(a, arrayB):
         answer = max(answer, a)
     if is_valid(b, arrayA):
         answer = max(answer, b)
-    
+        
     return answer
