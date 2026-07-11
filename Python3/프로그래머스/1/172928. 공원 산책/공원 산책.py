6b008aca977b2d9ad7f05c6b4bf52907c0281323
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-12 00:08:39 +0900

    [level 1] Title: 공원 산책, Time: 0.12 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ Python3/프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,11 +1,10 @@
 def solution(park, routes):
-    direct = {
-        'N':(-1, 0),
-        'S':(1, 0),
-        'W':(0, -1),
-        'E':(0, 1)
+    direction_dict = {
+        'N': (-1, 0),
+        'S': (1, 0),
+        'W': (0, -1),
+        'E': (0, 1)
     }
-    
     row = len(park)
     column = len(park[0])
     x, y = 0, 0
@@ -13,23 +12,20 @@ def solution(park, routes):
         for j in range(column):
             if park[i][j] == 'S':
                 x, y = i, j
-    
+            
     for route in routes:
-        direction, cnt = route.split()
-        cnt = int(cnt)
-        
-        dx, dy = direct[direction]
         can_move = True
-        
+        direct, cnt = route.split()
+        cnt = int(cnt)
+        dx, dy = direction_dict[direct] 
         nx, ny = x, y
+        
         for _ in range(cnt):
             nx += dx
-            ny += dy 
-            if(nx<0 or nx>=row or ny<0 or ny>=column or park[nx][ny]=='X'):
+            ny += dy
+            if nx<0 or nx>=row or ny<0 or ny>=column or park[nx][ny]=='X':
                 can_move = False
-                break 
-        
-        if can_move: 
+                break
+        if can_move:
             x, y = nx, ny
-
     return [x, y]
