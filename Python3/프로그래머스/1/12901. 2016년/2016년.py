01fdb65424945b3229eb6ab06375a8b85560f1e0
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-13 14:48:02 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/12901. 2016년/2016년.py
+++ Python3/프로그래머스/1/12901. 2016년/2016년.py
@@ -1,4 +1,10 @@
-date = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
-day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def solution(a, b):
-    return date[(sum(day[:a-1])+b)%7]
+    #1월1일 금요일
+    day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    month_sum = 0
+    for i in range(a-1):
+        month_sum += month[i]
+    calen = month_sum + b - 1
+    return day[calen%len(day)]
--- Python3/프로그래머스/1/12901. 2016년/README.md
+++ Python3/프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 9.19 MB, 시간: 0.00 ms
+메모리: 11.5 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 21:23:46
+2026년 07월 13일 14:47:59
 
 ### 문제 설명
 
