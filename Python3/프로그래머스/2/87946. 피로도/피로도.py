15f618d160918c100813189eed188546938da6d6
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-15 23:03:50 +0900

    [level 2] Title: 피로도, Time: 22.82 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/87946. 피로도/피로도.py
+++ Python3/프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,15 @@
 def solution(k, dungeons):
     visited = [False] * len(dungeons)
     answer = 0
-    def dfs(current_k, cnt):
+    def dfs(current, cnt):
         nonlocal answer
         answer = max(answer, cnt)
         for i in range(len(dungeons)):
             need = dungeons[i][0]
             used = dungeons[i][1]
-            if not visited[i] and current_k >= need:
+            if not visited[i] and current >= need:
                 visited[i] = True
-                dfs(current_k - used, cnt + 1)
+                dfs(current-used, cnt+1)
                 visited[i] = False
     dfs(k, 0)
     return answer
