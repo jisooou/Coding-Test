ea7e85742ada510243da7def1bf131eddd01a19e
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-16 12:24:49 +0900

    [level 2] Title: 귤 고르기, Time: 15.61 ms, Memory: 23.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,12 +1,18 @@
-from collections import Counter
 def solution(k, tangerine):
-    tang_dict = Counter(tangerine)
+    #서로 다른 종류 최솟값
+    collect = {}
+    for i in tangerine:
+        if i not in collect:
+            collect[i] = 1
+        else:
+            collect[i] += 1   
+    sort_collect = sorted(collect.values(), reverse=True)
+    
     total = 0
     answer = 0
-    tang_cnt = sorted(tang_dict.values(), reverse=True)
-    for c in tang_cnt:
-        total += c
+    for i in sort_collect: 
+        total += i 
         answer += 1
         if total >= k:
             return answer
-            
+    return answer
