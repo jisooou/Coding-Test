d69cb093a9271e8347c48cb5df8945371bb3c8ca
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-17 14:40:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.91 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ Python3/프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,28 +1,24 @@
-from collections import deque
 def solution(n, wires):
+    #하나를 끊어서 개수 비슷하게->개수 차이(절대값)
     answer = n
-    for cut_x, cut_y in wires:
+    for cut_a, cut_b in wires:
         graph = [[] for _ in range(n+1)]
-        for a,b in wires:
-            if cut_x == a and cut_y == b:
+        for a, b in wires:
+            if cut_a == a and cut_b == b:
                 continue
-            graph[a].append(b)
+            graph[a].append(b) 
             graph[b].append(a)
-            
-        queue = deque()
+        
         visited = [False] * (n+1)
-        queue.append(1)
-        visited[1] = True
-        cnt = 1
-
-        while queue:
-            current = queue.popleft() 
+        def dfs(current):
+            visited[current] = True
+            cnt = 1
             for nxt in graph[current]:
                 if not visited[nxt]:
-                    visited[nxt] = True
-                    queue.append(nxt)
-                    cnt += 1
-
-        remain = n-cnt
-        answer = min(answer, abs(cnt-remain))
+                    cnt += dfs(nxt)
+            return cnt
+        
+        count = dfs(1)
+        remain = n - count
+        answer = min(answer, abs(count-remain))
     return answer
