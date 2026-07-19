6453b0b2ed63954bae953aa38bed82716f5085fa
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-19 16:25:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 21.73 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ Python3/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,28 +1,28 @@
 def solution(s):
+    def is_correct(operand):
+        stack = []
+        pair = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+        for i in operand:
+            if i in '([{':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                if stack[-1]!=pair[i]:
+                    return False
+                stack.pop()
+        if not stack:
+            return True
+        return False
+            
     cnt = 0
     for i in range(len(s)):
-        operand = s[i:] + s[:i]
-        
-        if is_valid(operand): 
+        rotate = s[i:] + s[:i]
+        if is_correct(rotate):
             cnt += 1
     return cnt
-    
-def is_valid(operand):
-    stack = []
-    pairs = {
-        ']' : '[',
-        ')' : '(', 
-        '}' : '{'
-    }
-    for o in operand:
-        if o in '[({':
-            stack.append(o)
-        else:
-            if stack and stack[-1] == pairs[o]:
-                stack.pop()
-            else:
-                return False
-    if not stack:
-        return True
-    else:
-        return False
+        
