dae9ff06e48ac7cca197283e3e2c6bb6c17be5f5
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-22 16:06:42 +0900

    [level 2] Title: 가장 큰 수, Time: 36.37 ms, Memory: 22.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ Python3/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,8 @@
 def solution(numbers):
-    #문자열로 리턴
-    str_num = list(map(str, numbers))
-    lst = sorted(str_num, reverse=True, key=lambda x: x*3) 
-    if lst[0] == '0':
+    answer = ''
+    number = sorted(numbers, key=lambda x: str(x)*3, reverse=True)
+    for i in number:
+        answer += str(i)
+    if answer[0] == '0':
         return '0'
-    return ''.join(lst)
+    return answer
