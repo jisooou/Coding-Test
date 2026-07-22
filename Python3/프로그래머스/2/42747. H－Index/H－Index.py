71a595d0b9f6a955ccb898cb7e33c81ccc4472b9
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-22 15:03:55 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,7 +1,8 @@
 def solution(citations):
-    citations.sort() 
+    #h번이상, h편이상
+    citations.sort() #[0, 1, 3, 5, 6]
     for i in range(len(citations)):
-        remain = len(citations) - i
-        if remain <= citations[i]:
+        remain = len(citations) - i 
+        if citations[i] >= remain:
             return remain
     return 0
