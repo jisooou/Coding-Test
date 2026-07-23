f0e2e1e09b927fd333c4a216000c4c3f0b7c729a
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-23 22:07:09 +0900

    [level 2] Title: 문자열 압축, Time: 1.34 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ Python3/프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,4 +1,5 @@
 def solution(s):
+    #가장 짧을 때 구하기
     answer = len(s)
     for unit in range(1, len(s)//2+1):
         prev = s[0:unit]
@@ -6,7 +7,7 @@ def solution(s):
         compress = ''
         for i in range(unit, len(s), unit):
             current = s[i:i+unit]
-            if prev == current:
+            if prev == current: 
                 cnt += 1
             else:
                 if cnt > 1:
@@ -21,5 +22,3 @@ def solution(s):
             compress += prev
         answer = min(answer, len(compress))
     return answer
-                
-            
