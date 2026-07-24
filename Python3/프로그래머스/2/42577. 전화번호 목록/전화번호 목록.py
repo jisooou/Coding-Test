c2facad1e4bc53fffcf98a8fc2ea384671e25c9f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-24 14:31:36 +0900

    [level 2] Title: 전화번호 목록, Time: 49.04 ms, Memory: 29 MB -BaekjoonHub

--- Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ Python3/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,5 +1,5 @@
 def solution(phone_book):
-    sort_phone = phone_book.sort()
+    phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
