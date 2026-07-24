0443f61ca558638b7afdb1857625a44fb84f6773
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-24 13:30:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.27 ms, Memory: 13.6 MB -BaekjoonHub

--- Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,16 +1,11 @@
 def solution(s):
-    #다없어지면 1, 아니면 0 
     stack = []
-    for word in s:
-        if not stack:
-            stack.append(word)
-        else: 
-            if stack and stack[-1]==word:
-                stack.pop()
-            else:
-                stack.append(word)
-    
-    if not stack:
+    for i in s:
+        if stack and stack[-1]==i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if not stack: 
         return 1
     else:
         return 0
