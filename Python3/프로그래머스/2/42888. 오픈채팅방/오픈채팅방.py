e1e583451ca57c2b2fa9e4f9565602ca5ea907b5
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-25 16:13:26 +0900

    [level 2] Title: 오픈채팅방, Time: 73.54 ms, Memory: 41.7 MB -BaekjoonHub

--- Python3/프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ Python3/프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,27 +1,24 @@
 def solution(record):
-    #1. 변수 정하기, 입력값 나누기 
-    #2. 아이디가 같으면 마지막 이름으로 변경  
-    #3. 변수에 따른 한국어 출력
-    name_list = {}
+    name_lst = {}
     result = []
-    
-    for i in record: 
-        parts = i.split()
-        type = parts[0]
+    for i in range(len(record)):
+        parts = record[i].split()
+        command = parts[0]
         id = parts[1]
         
-        if type=='Enter' or type=='Change':
+        if command == 'Enter' or command == 'Change':
             name = parts[2]
-            name_list[id] = name
+            name_lst[id] = name
             
-    for i in record:
-        parts = i.split()
-        type = parts[0]
+    for i in range(len(record)):
+        parts = record[i].split()
+        command = parts[0]
         id = parts[1]
         
-        if type=='Enter':
-            result.append(name_list[id]+'님이 들어왔습니다.')
-        elif type=='Leave':
-            result.append(name_list[id] +'님이 나갔습니다.')
+        if command == 'Enter':
+            result.append(name_lst[id] + '님이 들어왔습니다.')
+        elif command == 'Leave':
+            result.append(name_lst[id] + '님이 나갔습니다.')
     return result
+            
     
