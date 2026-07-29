1ec13bc77c50cc920b253f87e99400c2a3877e4f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-07-29 23:31:35 +0900

    [level 4] Title: 도둑질, Time: 255.80 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ Python3/프로그래머스/4/42897. 도둑질/도둑질.py
@@ -0,0 +1,16 @@
+def solution(money):
+    #첫번째집 O
+    dp1 = [0] * len(money)
+    dp1[0] = money[0]
+    dp1[1] = money[0]
+    for i in range(2, len(money)-1):
+        dp1[i] = max(dp1[i-2]+money[i], dp1[i-1])
+        
+    #첫번째집 X
+    dp2 = [0] * len(money)
+    dp2[0] = 0
+    dp2[1] = money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i-1], dp2[i-2]+money[i])
+        
+    return max(dp1[len(money)-2], dp2[len(money)-1])
