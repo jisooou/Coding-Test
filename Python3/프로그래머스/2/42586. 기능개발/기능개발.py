c11d0aacb3928c67ee128d702d0bb2705fcd2985
jisooou <87888800+jisooou@users.noreply.github.com>
2026-08-01 13:44:43 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/42586. 기능개발/기능개발.py
+++ Python3/프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,16 +2,16 @@ def solution(progresses, speeds):
     lst = []
     for progress, speed in zip(progresses, speeds):
         remain = 100-progress
-        if remain % speed == 0:
+        if remain%speed==0:
             lst.append(remain//speed)
         else:
             lst.append(remain//speed+1)
-
+    
     before = lst[0]
     cnt = 1
     answer = []
-    for i in range(1, len(lst)): 
-        if lst[i] <= before:
+    for i in range(1, len(lst)):
+        if before >= lst[i]:
             cnt += 1
         else:
             answer.append(cnt)
@@ -19,3 +19,4 @@ def solution(progresses, speeds):
             cnt = 1
     answer.append(cnt)
     return answer
+            
