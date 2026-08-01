3a3a445ff86083b531a29f5108e1eec5d388ae51
jisooou <87888800+jisooou@users.noreply.github.com>
2026-08-01 15:06:01 +0900

    [level 3] Title: 가장 먼 노드, Time: 16.87 ms, Memory: 21.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,19 @@
+from collections import deque
+def solution(n, edge):
+    graph = [[] for _ in range(n+1)]
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    distance = [-1]*(n+1)
+    distance[1] = 0
+    queue = deque()
+    queue.append(1)
+    while queue: 
+        current = queue.popleft()
+        for nxt in graph[current]:
+            if distance[nxt] == -1:
+                distance[nxt] = distance[current]+1
+                queue.append(nxt)
+    max_answer = max(distance)
+    return distance.count(max_answer)
