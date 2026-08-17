46760a52f35d1f16d5c506dd676f08cf6f148d3f
jisooou <87888800+jisooou@users.noreply.github.com>
2026-08-17 23:20:03 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ Python/프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,11 +1,11 @@
 def solution(skill, skill_trees):
     answer = 0
-    for skill_tree in skill_trees:
+    for word in skill_trees:
         temp = ''
-        for ch in skill_tree:
+        for ch in word:
             if ch in skill:
                 temp += ch
-                
+            
         if skill.startswith(temp):
             answer += 1
     return answer
