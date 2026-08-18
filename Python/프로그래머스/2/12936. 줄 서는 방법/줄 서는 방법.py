d0cdfd24418c80b625955fbe76eed963880c18d6
jisooou <87888800+jisooou@users.noreply.github.com>
2026-08-18 21:04:42 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 11.3 MB -BaekjoonHub

--- Python/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ Python/프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -3,10 +3,10 @@ def solution(n, k):
     result = []
     numbers = [i for i in range(1, n+1)]
     k = k - 1
-    while numbers: 
+    while numbers:
         block = math.factorial(len(numbers)-1)
-        idx = k // block
+        idx = k // block 
         result.append(numbers.pop(idx))
         
-        k = k % block
+        k %= block
     return result
