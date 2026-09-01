faaf22247766ae338bfcf7a4b6268f68394dfcf0
jisooou <87888800+jisooou@users.noreply.github.com>
2026-09-02 01:06:44 +0900

    [level 2] Title: 타겟 넘버, Time: 114.53 ms, Memory: 11.7 MB -BaekjoonHub

--- Python/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,12 @@
 def solution(numbers, target):
-    cnt = 0
+    cnt = 0 
     def dfs(idx, total):
         nonlocal cnt
         if idx == len(numbers):
             if total == target:
                 cnt += 1
-            return
-        
+            return 
         dfs(idx+1, total+numbers[idx])
         dfs(idx+1, total-numbers[idx])
-    
     dfs(0, 0)
     return cnt
